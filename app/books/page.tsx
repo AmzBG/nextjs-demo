fb8e0d0b7304113f239370280b2dcbdf55a902e7
AmzBG <ameer.boughanem@gmail.com>
2026-06-19 02:00:39 +0300

    feat: sync book genre filter with query params

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,9 +1,24 @@
 import { getAllBooks, getAllAuthors } from '@/lib/data';
 import BooksClient from '@/components/BooksClient';
 
-export default function BooksPage() {
+interface BooksPageProps {
+  searchParams: Promise<{
+    genre?: string | string[];
+  }>;
+}
+
+export default async function BooksPage({ searchParams }: BooksPageProps) {
   const books = getAllBooks();
   const authors = getAllAuthors();
+  const params = await searchParams;
+  const genreParam = params.genre;
+  const selectedGenre = Array.isArray(genreParam) ? genreParam[0] : genreParam;
 
-  return <BooksClient initialBooks={books} authors={authors} />;
+  return (
+    <BooksClient
+      initialBooks={books}
+      authors={authors}
+      selectedGenre={selectedGenre ?? 'all'}
+    />
+  );
 }
